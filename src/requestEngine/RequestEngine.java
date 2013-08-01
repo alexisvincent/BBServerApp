11025b7b7c73e82760435c83e04799c2eface355
Alexis Vincent <alexisjohnvincent@gmail.com>
2013-08-02 00:32:17 +0200

    Edited how Election Profiles work

--- src/requestEngine/RequestEngine.java
+++ src/requestEngine/RequestEngine.java
@@ -1,7 +1,9 @@
 package requestEngine;
 
+import java.util.ArrayList;
 import networking.Request;
 import networking.Responce;
+import objects.Candidate;
 import org.jdom2.Document;
 import org.jdom2.Element;
 import sqlEngine.SQLEngine;
@@ -26,6 +28,8 @@ public class RequestEngine {
 
         Responce responce;
         Element responceElement = new Element("Responce");
+        responceElement.setAttribute("ResponceCode", "400");
+        responceElement.setAttribute("ResponceDiscription", "Invalid Request");
 
         if (request.getRequestType().equals("Login") && request.getFrom().equals("RegistrationApp")) {
 
@@ -41,12 +45,14 @@ public class RequestEngine {
             }
 
         } else if (request.getRequestType().equals("VotersKey") && request.getFrom().equals("RegistrationApp")) {
-            
-            String votersKey = sqlEngine.getVotersKey(request);
-            
+
+            String idNumber = rootElement.getAttributeValue("idNumber");
+            String firstName = rootElement.getAttributeValue("firstName");
+            String votersKey = sqlEngine.getVotersKey(idNumber, firstName);
+
             responceElement = new Element("Responce");
             responceElement.setAttribute("VotersKey", votersKey);
-            
+
             if (votersKey.equals("")) {
                 responceElement.setAttribute("ResponceCode", "400");
                 responceElement.setAttribute("ResponceDiscription", "No such details");
@@ -54,7 +60,45 @@ public class RequestEngine {
                 responceElement.setAttribute("ResponceCode", "200");
                 responceElement.setAttribute("ResponceDiscription", "OK");
             }
-            
+
+        } else if (request.getRequestType().equals("Candidates")) {
+
+            ArrayList<Candidate> candidates = sqlEngine.getCandidates();
+
+            responceElement = new Element("Responce");
+
+            int counter = 1;
+            for (Candidate candidate : candidates) {
+                Element candidateElement = new Element("Candidate" + counter++);
+                candidateElement.setAttribute("id", candidate.getId());
+                candidateElement.setAttribute("name", candidate.getName());
+                candidateElement.setAttribute("info", candidate.getInfo());
+                responceElement.addContent(candidateElement);
+            }
+
+            responceElement.setAttribute("ResponceCode", "200");
+            responceElement.setAttribute("ResponceDiscription", "OK");
+            responceElement.setAttribute("CandidatesCount", candidates.size() + "");
+        } else if (request.getRequestType().equals("Vote") && request.getFrom().equals("VotingApp")) {
+
+            String voterkey = rootElement.getAttributeValue("VoterKey");
+            String candidateID = rootElement.getAttributeValue("CandidateID");
+            String status = sqlEngine.vote(voterkey, candidateID);
+
+            if (status.equals("Successfull")) {
+                responceElement.setAttribute("ResponceCode", "200");
+                responceElement.setAttribute("ResponceDiscription", "OK");
+            } else if (status.equals("AlreadyVoted")) {
+                responceElement.setAttribute("ResponceCode", "300");
+                responceElement.setAttribute("ResponceDiscription", "Already Voted");
+            } else if (status.equals("IncorrectKey")) {
+                responceElement.setAttribute("ResponceCode", "310");
+                responceElement.setAttribute("ResponceDiscription", "Incorrect Key");
+            } else {
+                responceElement.setAttribute("ResponceCode", "400");
+                responceElement.setAttribute("ResponceDiscription", "Failed");
+            }
+
         }
 
         responce = new Responce(new Document(responceElement), request.getSocket());
--- src/sqlEngine/SQLEngine.java
+++ src/sqlEngine/SQLEngine.java
@@ -7,13 +7,13 @@ import java.sql.PreparedStatement;
 import java.sql.ResultSet;
 import java.sql.SQLException;
 import java.sql.Statement;
+import java.util.ArrayList;
 import java.util.Properties;
 import java.util.logging.Level;
 import java.util.logging.Logger;
-import networking.Request;
 import objects.Address;
+import objects.Candidate;
 import objects.Voter;
-import org.jdom2.Element;
 
 /**
  *
@@ -40,14 +40,50 @@ public class SQLEngine {
         }
     }
 
-    public String getVotersKey(Request request) {
-        String votersKey = "";
-        String idNumber = "";
-        String firstName = "";
+    public String vote(String voterID, String candidateID) {
+        String status = "";
 
-        Element rootElement = request.getRootElement();
-        idNumber = rootElement.getAttributeValue("idNumber");
-        firstName = rootElement.getAttributeValue("firstName");
+        String sql = "SELECT votersID, candidateID FROM tblVotes WHERE votersID='" + voterID + "'";
+
+        ResultSet resultSet = executeQuery(sql);
+        try {
+            if (resultSet.next()) {
+                if (resultSet.getString("candidateID") == null) {
+                    sql = "UPDATE tblVotes SET candidateID='" + candidateID + "' WHERE votersID='" + voterID + "'";
+                    executeUpdate(sql);
+                    status = "Successfull";
+                } else {
+                    status = "AlreadyVoted";
+                }
+                
+            } else {
+                status = "IncorrectKey";
+            }
+        } catch (SQLException ex) {
+            Logger.getLogger(SQLEngine.class.getName()).log(Level.SEVERE, null, ex);
+        }
+
+        return status;
+    }
+
+    public ArrayList<Candidate> getCandidates() {
+        ArrayList<Candidate> candidates = new ArrayList<>();
+
+        String sql = "SELECT id, name, info, image FROM tblCandidates";
+        ResultSet resultSet = executeQuery(sql);
+        try {
+            while (resultSet.next()) {
+                candidates.add(new Candidate(resultSet.getString("id"), resultSet.getString("name"), resultSet.getString("info"), null));
+            }
+        } catch (SQLException ex) {
+            Logger.getLogger(SQLEngine.class.getName()).log(Level.SEVERE, null, ex);
+        }
+
+        return candidates;
+    }
+
+    public String getVotersKey(String idNumber, String firstName) {
+        String votersKey = "";
 
         if (!idNumber.equals("") && !firstName.equals("")) {
             String sql = "SELECT * FROM tblVoterRegistry WHERE firstName='" + firstName + "' AND idNumber='" + idNumber + "'";
@@ -69,7 +105,7 @@ public class SQLEngine {
     private ResultSet executeQuery(String sql) {
         ResultSet resultSet = null;
         try {
-            PreparedStatement preparedStatement = connection.prepareStatement(sql, 
+            PreparedStatement preparedStatement = connection.prepareStatement(sql,
                     ResultSet.TYPE_SCROLL_INSENSITIVE, ResultSet.CONCUR_READ_ONLY);
             resultSet = preparedStatement.executeQuery();
         } catch (SQLException ex) {
@@ -77,7 +113,7 @@ public class SQLEngine {
         }
         return resultSet;
     }
-    
+
     private boolean executeUpdate(String sql) {
         try {
             Statement statement = connection.createStatement();
@@ -87,7 +123,7 @@ public class SQLEngine {
             Logger.getLogger(SQLEngine.class.getName()).log(Level.SEVERE, null, ex);
             return false;
         }
-        
+
     }
 
     private void createDatebase(String databaseName, String username, String password) {
@@ -115,17 +151,17 @@ public class SQLEngine {
         String tblVotes = "CREATE TABLE tblVotes "
                 + "(votersID VARCHAR(32), "
                 + " encryptionKey VARCHAR(32), "
-                + " partyID VARCHAR(15), "
+                + " candidateID VARCHAR(15), "
                 + " timestamp VARCHAR(15), "
                 + " status VARCHAR(15), "
                 + " PRIMARY KEY ( votersID ))";
 
         String tblCandidates = "CREATE TABLE tblCandidates "
-                + "(candidateID VARCHAR(15), "
+                + "(id VARCHAR(15), "
                 + " name VARCHAR(30), "
-                + " bio VARCHAR(120), "
-                + " emblem VARCHAR(255), "
-                + " PRIMARY KEY ( candidateID ))";
+                + " info VARCHAR(120), "
+                + " image VARCHAR(255), "
+                + " PRIMARY KEY ( id ))";
 
         try {
 
@@ -148,39 +184,88 @@ public class SQLEngine {
             Logger.getLogger(SQLEngine.class.getName()).log(Level.SEVERE, null, ex);
         }
     }
-    
+
     public void populateDatabase() {
         Voter voter = new Voter();
-        
         voter.setFirstName("Alexis");
         voter.setMiddleNames("John");
         voter.setSurname("Vincent");
         voter.setIdNumber("1234567891234");
         voter.setAddress(new Address("51 Kirkia Street", "", "Heldervue", "Somerset West", "Western Cape"));
         addVoter(voter);
+
+        Candidate candidate = new Candidate();
+        candidate.setId("1");
+        candidate.setName("African National Congress");
+        candidate.setInfo("Haha these guys suck");
+        addCandidate(candidate);
+
+        candidate.setId("2");
+        candidate.setName("Democratic Alliance");
+        candidate.setInfo("These are slightly better");
+        addCandidate(candidate);
+
+        candidate.setId("3");
+        candidate.setName("Alexis is the Best");
+        candidate.setInfo("The name says it all");
+        addCandidate(candidate);
+        
+        candidate.setId("4");
+        candidate.setName("Blah Blie Blue Bal");
+        candidate.setInfo("The name says it all");
+        addCandidate(candidate);
+        
+        candidate.setId("5");
+        candidate.setName("I want to go to the moon");
+        candidate.setInfo("The name says it all");
+        addCandidate(candidate);
+        
+        candidate.setId("6");
+        candidate.setName("Im bored");
+        candidate.setInfo("The name says it all");
+        addCandidate(candidate);
+        
+        candidate.setId("7");
+        candidate.setName("O.o");
+        candidate.setInfo("The name says it all");
+        addCandidate(candidate);
     }
-    
+
+    public void addCandidate(Candidate candidate) {
+        String sql = "INSERT INTO tblCandidates VALUES('"
+                + candidate.getId() + "','"
+                + candidate.getName() + "','"
+                + candidate.getInfo() + "','"
+                + "IMAGECODE')";
+
+        executeUpdate(sql);
+    }
+
     public void addVoter(Voter voter) {
         if (voter.getVotersID().equals("")) {
             voter.setVotersID("123456789");
         } else if (voter.getEncryptionKey().equals("123456789")) {
-            
         }
         String sql = "INSERT INTO tblVoterRegistry VALUES('"
-                + voter.getIdNumber()+"','"
-                + voter.getFirstName()+"','"
-                + voter.getMiddleNames()+"','"
-                + voter.getSurname()+"','"
-                + voter.getAddress().getAddressLine1()+"','"
-                + voter.getAddress().getAddressLine2()+"','"
-                + voter.getAddress().getSuburb()+"','"
-                + voter.getAddress().getCity()+"','"
-                + voter.getAddress().getProvince()+"','"
-                + voter.getVotersID()+"','"
-                + voter.getEncryptionKey()+"')";
-        
+                + voter.getIdNumber() + "','"
+                + voter.getFirstName() + "','"
+                + voter.getMiddleNames() + "','"
+                + voter.getSurname() + "','"
+                + voter.getAddress().getAddressLine1() + "','"
+                + voter.getAddress().getAddressLine2() + "','"
+                + voter.getAddress().getSuburb() + "','"
+                + voter.getAddress().getCity() + "','"
+                + voter.getAddress().getProvince() + "','"
+                + voter.getVotersID() + "','"
+                + voter.getEncryptionKey() + "')";
+
         executeUpdate(sql);
-        
+
+        sql = "INSERT INTO tblVotes(votersID) VALUES('"
+                + voter.getVotersID() + "')";
+
+        executeUpdate(sql);
+
     }
 
     private void connect(String databaseName, String username, String password) {
